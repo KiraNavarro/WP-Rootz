4248520c0c3b479672b6786173b4672ff1a98443
Jess Navarro <kira.navarro@gmail.com>
2018-01-16 14:33:34 -0600

    Change theme name

--- wp-content/themes/wp-rootz/inc/customizer.php
+++ wp-content/themes/wp-rootz/inc/customizer.php
@@ -11,7 +11,7 @@
 		$wp_customize->add_control(new WP_Customize_Image_Control(
 			$wp_customize, 'logo_image',
 			array(
-				'label' => __('Site Logo', 'dexos'),
+				'label' => __('Site Logo', 'wp-rootz'),
 				'section' => 'title_tagline',
 				'settings' => 'logo_image',
 				'priority' => 10
@@ -96,31 +96,31 @@
 		
 		//Set section 
 		$wp_customize->add_section('about', array(
-			'title' => __('Sobre Nosotros', 'dexos'),
-			'description' => sprintf(__('Options for About us', 'dexos')),
+			'title' => __('Sobre Nosotros', 'wp-rootz'),
+			'description' => sprintf(__('Options for About us', 'wp-rootz')),
 			'priority' => 130
 		));
 		
 		// Title
 		$wp_customize->add_setting('about_heading', array(
-			'default' => _x('Sobre Nosotros', 'dexos'),
+			'default' => _x('Sobre Nosotros', 'wp-rootz'),
 			'type' => 'theme_mod'
 		));
 		
 		$wp_customize->add_control('about_heading', array(
-			'label' => __('Título', 'dexos'),
+			'label' => __('Título', 'wp-rootz'),
 			'section' => 'about',
 			'priority' => 1
 		));
 		
 		// Description Parrafo 1
 		$wp_customize->add_setting('about_description1', array(
-			'default' => _x('Esta es la descripción.', 'dexos'),
+			'default' => _x('Esta es la descripción.', 'wp-rootz'),
 			'type' => 'theme_mod'
 		));
 		
 		$wp_customize->add_control('about_description1', array(
-			'label' => __('Descripción 1', 'dexos'),
+			'label' => __('Descripción 1', 'wp-rootz'),
 			'type' => 'textarea',
 			'section' => 'about',
 			'priority' => 2
@@ -128,12 +128,12 @@
 		
 		// Description Parrafo 2
 		$wp_customize->add_setting('about_description2', array(
-			'default' => _x('Esta es la descripción.', 'dexos'),
+			'default' => _x('Esta es la descripción.', 'wp-rootz'),
 			'type' => 'theme_mod'
 		));
 		
 		$wp_customize->add_control('about_description2', array(
-			'label' => __('Descripción 2', 'dexos'),
+			'label' => __('Descripción 2', 'wp-rootz'),
 			'type' => 'textarea',
 			'section' => 'about',
 			'priority' => 3
@@ -148,7 +148,7 @@
 		$wp_customize->add_control(new WP_Customize_Image_Control(
 			$wp_customize, 'about_image',
 			array(
-				'label' => __('Imagen', 'dexos'),
+				'label' => __('Imagen', 'wp-rootz'),
 				'section' => 'about',
 				'settings' => 'about_image',
 				'priority' => 4
@@ -159,43 +159,43 @@
 		
 		//Set section
 		$wp_customize->add_section('contact', array(
-			'title' => __('Contacto', 'dexos'),
-			'description' => sprintf(__('Options for Contact us', 'dexos')),
+			'title' => __('Contacto', 'wp-rootz'),
+			'description' => sprintf(__('Options for Contact us', 'wp-rootz')),
 			'priority' => 140
 		));
 		
 		// Title
 		$wp_customize->add_setting('contact_heading', array(
-			'default' => _x('Contacto', 'dexos'),
+			'default' => _x('Contacto', 'wp-rootz'),
 			'type' => 'theme_mod'
 		));
 		
 		$wp_customize->add_control('contact_heading', array(
-			'label' => __('Título', 'dexos'),
+			'label' => __('Título', 'wp-rootz'),
 			'section' => 'contact',
 			'priority' => 1
 		));
 		
 		// Address 
 		$wp_customize->add_setting('contact_address', array(
-			'default' => _x('Av.Siempre Viva #12, Springfield', 'dexos'),
+			'default' => _x('Av.Siempre Viva #12, Springfield', 'wp-rootz'),
 			'type' => 'theme_mod'
 		));
 		
 		$wp_customize->add_control('contact_address', array(
-			'label' => __('Dirección', 'dexos'),
+			'label' => __('Dirección', 'wp-rootz'),
 			'section' => 'contact',
 			'priority' => 1
 		));
 		
 		// Phone 
 		$wp_customize->add_setting('contact_phone', array(
-			'default' => _x('55-2654-8745', 'dexos'),
+			'default' => _x('55-2654-8745', 'wp-rootz'),
 			'type' => 'theme_mod'
 		));
 		
 		$wp_customize->add_control('contact_phone', array(
-			'label' => __('Teléfono', 'dexos'),
+			'label' => __('Teléfono', 'wp-rootz'),
 			'section' => 'contact',
 			'priority' => 1
 		));
@@ -204,31 +204,31 @@
 		
 		//Set section
 		$wp_customize->add_section('social_networks', array(
-			'title' => __('Redes Sociales', 'dexos'),
-			'description' => sprintf(__('Options for Social Networks', 'dexos')),
+			'title' => __('Redes Sociales', 'wp-rootz'),
+			'description' => sprintf(__('Options for Social Networks', 'wp-rootz')),
 			'priority' => 140
 		));
 		
 		// Facebook
 		$wp_customize->add_setting('facebook', array(
-			'default' => _x('https://www.facebook.com/', 'dexos'),
+			'default' => _x('https://www.facebook.com/', 'wp-rootz'),
 			'type' => 'theme_mod'
 		));
 		
 		$wp_customize->add_control('facebook', array(
-			'label' => __('Facebook', 'dexos'),
+			'label' => __('Facebook', 'wp-rootz'),
 			'section' => 'social_networks',
 			'priority' => 1
 		));
 		
 		// Facebook
 		$wp_customize->add_setting('linkedin', array(
-			'default' => _x('https://www.linkedin.com/', 'dexos'),
+			'default' => _x('https://www.linkedin.com/', 'wp-rootz'),
 			'type' => 'theme_mod'
 		));
 		
 		$wp_customize->add_control('linkedin', array(
-			'label' => __('LinkedIn', 'dexos'),
+			'label' => __('LinkedIn', 'wp-rootz'),
 			'section' => 'social_networks',
 			'priority' => 1
 		));
@@ -236,25 +236,25 @@
 		/* Team 
 		$wp_customize->add_panel('equipo', array(
 			'priority'       => 131,
-			'title'          => __('Equipo', 'dexos'),
-			'description'    => __('Set team info.', 'dexos'),
+			'title'          => __('Equipo', 'wp-rootz'),
+			'description'    => __('Set team info.', 'wp-rootz'),
 		));
 		
 		$wp_customize->add_section( 'doctor', array(
 			'priority'       => 1,
-			'title'          => __('Doctor', 'dexos'),
-			'description'    =>  __('Doctor settings', 'dexos'),
+			'title'          => __('Doctor', 'wp-rootz'),
+			'description'    =>  __('Doctor settings', 'wp-rootz'),
 			'panel'  => 'equipo',
 		));
 		
 		// Doctor Nombre
 		$wp_customize->add_setting('doctor-nombre', array(
-			'default' => _x('Esta es la descripción.', 'dexos'),
+			'default' => _x('Esta es la descripción.', 'wp-rootz'),
 			'type' => 'theme_mod'
 		));
 			
 		$wp_customize->add_control('doctor-nombre', array(
-			'label' => __('Nombre', 'dexos'),
+			'label' => __('Nombre', 'wp-rootz'),
 			'type' => 'theme_mod',
 			'section' => 'doctor',
 			'priority' => 1
