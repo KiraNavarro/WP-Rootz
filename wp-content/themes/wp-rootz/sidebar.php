c5576d228d478ee936b25bac02a00e597f108335
Jess Navarro <kira.navarro@gmail.com>
2018-01-16 12:38:55 -0600

    Separate content in its own file

--- wp-content/themes/wp-rootz/sidebar.php
+++ wp-content/themes/wp-rootz/sidebar.php
@@ -1,5 +1,5 @@
 <aside class="col-sm-3 ml-sm-auto blog-sidebar">
-	<?php if (is_active_sidebar( 'sidebar' ) ) : 
-		dynamic_sidebar( 'sidebar' ); 
-	endif; ?>
+	<?php if (is_active_sidebar( 'sidebar' ) ) : ?>
+		<?php dynamic_sidebar( 'sidebar' ); ?>	
+	<?php endif; ?>
 </aside><!-- /.blog-sidebar -->
