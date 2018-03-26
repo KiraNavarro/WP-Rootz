fda2296ce2fecfc5efb7ab32fb15f5ac55048f87
Jess Navarro <kira.navarro@gmail.com>
2018-03-26 12:32:41 -0600

    Adjust contact form position

--- wp-content/themes/gemgas/page.php
+++ wp-content/themes/gemgas/page.php
@@ -1,25 +1,24 @@
 <?php get_header(); ?>	
 	<main role="main" class="container main">
-			<div class="blog-main">
-				<?php if( have_posts()) : ?>
-					<?php while( have_posts()) : the_post(); ?>
-						<div class="blog-post">
-							<h2 class="blog-post-title">
-								<?php the_title(); ?>
-							</h2>
-							
-							<?php the_content(); ?>
-						</div><!-- /.blog-post -->
-					<?php endwhile; ?>
-				<?php else : ?>
-					<p><?php __('No Page found'); ?></p>
-				<?php endif; ?>
-				<!-- Contact Form -->
-				<?php
-					echo do_shortcode( '[contact-form-7 id="144" title="Contact form 1"]'); 
-				?>
-			</div><!-- /.blog-main -->
-		
+		<div class="blog-main">
+			<?php if( have_posts()) : ?>
+				<?php while( have_posts()) : the_post(); ?>
+					<div class="blog-post">
+						<h2 class="blog-post-title">
+							<?php the_title(); ?>
+						</h2>
+						
+						<?php the_content(); ?>
+					</div><!-- /.blog-post -->
+				<?php endwhile; ?>
+			<?php else : ?>
+				<p><?php __('No Page found'); ?></p>
+			<?php endif; ?>
+		</div><!-- /.blog-main -->
+		<!-- Contact Form -->
+		<?php
+			echo do_shortcode( '[contact-form-7 id="144" title="Contact form 1"]'); 
+		?>
 	</main><!-- /.container -->
 	
 	<?php get_footer(); ?>
