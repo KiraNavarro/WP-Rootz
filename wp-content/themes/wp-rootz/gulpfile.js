5b3e5ff1ade73fc1a269a8a0229c615e4ca1eb73
Jess Navarro <kira.navarro@gmail.com>
2018-04-16 08:09:57 -0500

    Add Browsersync

--- wp-content/themes/wp-rootz/gulpfile.js
+++ wp-content/themes/wp-rootz/gulpfile.js
@@ -1,13 +1,26 @@
 var gulp = require('gulp');
 	sass = require('gulp-sass');
 	watch = require('gulp-watch');
+	browserSync = require('browser-sync').create();
 	
+gulp.task('browser-sync', () => {
+	var files = [
+		'./css/main.css',
+		'./*.php'
+	]
+	// Initialize Browsersync with php server
+	browserSync.init(files,{
+		proxy: "http://localhost:8888/wp-rootz"
+	});
+});
+
 gulp.task('sass', () => {
 	gulp.src('sass/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./css/'));
+		.pipe(gulp.dest('./css/'))
+		.pipe(browserSync.stream());
 });
 
-gulp.task('default', ['sass'], ()=>{
+gulp.task('default', ['sass', 'browser-sync'], () => {
 	return gulp.watch('sass/**/*.scss', ['sass']);
 });
