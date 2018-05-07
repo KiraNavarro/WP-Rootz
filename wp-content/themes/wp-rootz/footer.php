4c795bb98569c0aa5e7fba3ccec2be3bfcb21dd8
Jess Navarro <kira.navarro@gmail.com>
2018-05-07 16:12:04 -0500

    Images and adjustments

--- wp-content/themes/wp-rootz/footer.php
+++ wp-content/themes/wp-rootz/footer.php
@@ -9,5 +9,9 @@
 	================================================== -->
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="<?php bloginfo('template_url'); ?>/js/bootstrap.js"></script>
+	<!-- Responsive Tables -->
+	<script>
+		$( "table" ).wrap( "<div class='table-responsive'></div>" );
+	</script>
 </body>
 </html>
--- wp-content/themes/wp-rootz/header.php
+++ wp-content/themes/wp-rootz/header.php
@@ -6,18 +6,18 @@
 	<meta name="description" content="<?php bloginfo('descritpion'); ?>" >
 	<link rel="icon" href="../../../../favicon.ico">
 	<title>
-		<?php bloginfo('name'); ?> | 
+		<?php bloginfo('name'); ?> |
 		<?php is_front_page() ? bloginfo('description') : wp_title(); ?>
 	</title>
 	<!-- Bootstrap core CSS -->
 	<link href="<?php bloginfo('template_url'); ?>/css/bootstrap.css" rel="stylesheet">
-	
+
 	<!-- Font Awesome -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" integrity="sha384-3AB7yXWz4OeoZcPbieVW64vVXEwADiYyAEhwilzWsLw+9FgqpyjjStpPnpBO8o8S" crossorigin="anonymous">
 	<?php wp_head(); ?>
 	<style media="screen">
 		header {
-			background: url( 
+			background: url(
 				<?php if (is_front_page()) : ?>
 					<?php echo get_template_directory_uri().'/img/head-1.jpg' ?>
 				<?php elseif (is_page('soluciones')) : ?>
@@ -35,27 +35,72 @@
 				<?php else : ?>
 					<?php echo get_template_directory_uri().'/img/head-1.jpg' ?>
 				<?php endif; ?>) no-repeat;
-				
-			background-size: 100% 100%;
-			background-position: center;
+				background-size: cover;
+				background-position: center;
+		}
+		.blog-logo {
+			background: url(
+				<?php if (is_front_page()) : ?>
+					<?php echo get_template_directory_uri().'/img/site-logo.png' ?>
+				<?php elseif (is_page('soluciones')) : ?>
+					<?php echo get_template_directory_uri().'/img/site-logo.png' ?>
+				<?php elseif (is_page('administracion-de-flotas'))  : ?>
+					<?php echo get_template_directory_uri().'/img/site-logo.png' ?>
+				<?php elseif (is_page('ubiq-bus'))  : ?>
+					<?php echo get_template_directory_uri().'/img/site-logo.png' ?>
+				<?php elseif (is_page('ubiq-logistics'))  : ?>
+					<?php echo get_template_directory_uri().'/img/site-logo-2.png' ?>
+				<?php elseif (is_page('gpss'))  : ?>
+					<?php echo get_template_directory_uri().'/img/site-logo-2.png' ?>
+				<?php elseif (is_page('iot'))  : ?>
+					<?php echo get_template_directory_uri().'/img/site-logo-2.png' ?>
+				<?php else : ?>
+					<?php echo get_template_directory_uri().'/img/site-logo.png' ?>
+				<?php endif; ?>) no-repeat;
+				background-size: contain;
+		}
+		@media (max-width: 450px) {
+			header {
+				background: url(
+					<?php if (is_front_page()) : ?>
+						<?php echo get_template_directory_uri().'/img/movil-head.jpg' ?>
+					<?php elseif (is_page('soluciones')) : ?>
+						<?php echo get_template_directory_uri().'/img/movil-soluciones-head.jpg' ?>
+					<?php elseif (is_page('administracion-de-flotas'))  : ?>
+						<?php echo get_template_directory_uri().'/img/movil-administracion-head.jpg' ?>
+					<?php elseif (is_page('ubiq-bus'))  : ?>
+						<?php echo get_template_directory_uri().'/img/movil-bus-head.jpg' ?>
+					<?php elseif (is_page('ubiq-logistics'))  : ?>
+						<?php echo get_template_directory_uri().'/img/movil-logistics-head.jpg' ?>
+					<?php elseif (is_page('gpss'))  : ?>
+						<?php echo get_template_directory_uri().'/img/movil-gps-head.jpg' ?>
+					<?php elseif (is_page('iot'))  : ?>
+						<?php echo get_template_directory_uri().'/img/movil-iot-head.jpg' ?>
+					<?php else : ?>
+						<?php echo get_template_directory_uri().'/img/movil-head.jpg' ?>
+					<?php endif; ?>) no-repeat;
+				background-position: center;
+			}
+			.blog-logo {
+				background: url(<?php echo get_template_directory_uri().'/img/site-logo.png' ?>) no-repeat;
+				background-size: contain;
+			}
 		}
 	</style>
 </head>
 
 <body>
-	
+
 	<header>
 		<div class="blog-masthead">
 			<div class="container blog-header">
 				<div class="blog-logo">
-					<a href="<?php echo get_home_url(); ?>">
-						<img src="<?php echo get_template_directory_uri().'/img/site-logo.png' ?>" alt="Site logo">
-					</a>
+					<a href="<?php echo get_home_url(); ?>"></a>
 				</div>
 				<div class="blog-nav-content">
 					<div class="blog-info">
 						<span class="head-contact">Contáctenos: + (52 55) 5925 4309 </span>
-						<span class="head-social">Síguenos en: <a href="https://www.facebook.com/ubiqrastreo/" target="_blank"><img src="<?php echo get_template_directory_uri().'/img/facebook.png' ?>" alt="Site logo"> @ubiqmx</a></span>
+						<span class="head-social">Síguenos en: <a href="https://www.facebook.com/ubiqrastreo/" target="_blank"><img src="<?php echo get_template_directory_uri().'/img/facebook.png' ?>" alt="Facebook"> @ubiqmx</a></span>
 					</div>
 					<div class="blog-navbar">
 						<div class="container">
@@ -63,7 +108,7 @@
 								<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 									<span class="navbar-toggler-icon"></span>
 								</button>
-								
+
 								<div class="collapse navbar-collapse" id="navbarSupportedContent">
 									<?php
 										wp_nav_menu( array(
