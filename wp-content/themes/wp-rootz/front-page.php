c4419ad0c5e20abdbed44f6b6e25f32cb02a210a
Jess Navarro <kira.navarro@gmail.com>
2018-06-07 16:14:16 -0500

    Add front-page

--- wp-content/themes/wp-rootz/front-page.php
+++ wp-content/themes/wp-rootz/front-page.php
@@ -1,31 +1,60 @@
 <?php get_header(); ?>
-<section class="showcase">
-	<div class="container">
-		<h1><?php echo get_theme_mod('showcase_heading', 'Custom WP Rootz theme'); ?></h1>
-		<p><?php echo get_theme_mod('showcase_text', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'); ?></p>
-		<a href="<?php echo get_theme_mod('showcase_btn_url', 'http://test.com'); ?>" class="btn btn-primary btn-lg"><?php echo get_theme_mod('showcase_btn_text', 'Read More'); ?></a>
+<div class="orange-bg">
+	<div class="container front-page">
+		<h2>¿Qué distingue a Call2action, Empresas Felices?</h2>
+		<section class="intro-boxes">
+			<div class="intro-box">
+				<img src="<?php echo get_template_directory_uri().'/img/intro-box/intro-box1.png' ?>">
+				<h4>Te ahorramos tiempo</h4>
+				<p>Comprendemos tus necesidades y te proponemos la solución perfecta para tu evento de forma inmediata.</p>
+			</div>
+			<div class="intro-box">
+				<img src="<?php echo get_template_directory_uri().'/img/intro-box/intro-box2.png' ?>">
+				<h4>Te evitamos estrés</h4>
+				<p>Somos tu aliado antes, durante y después del evento. Nos aseguramos que cada detalle salga como tú quieres.¡Cuenta con nosotros!</p>
+			</div>
+			<div class="intro-box">
+				<img src="<?php echo get_template_directory_uri().'/img/intro-box/intro-box3.png' ?>">
+				<h4>Generamos resultados</h4>
+				<p>Logramos la interacción y el interés de tu audiencia. Diseñamos la conferencia ideal para que tus asistentes se sumen a los objetivos de tu evento.</p>
+			</div>
+			<div class="intro-box">
+				<img src="<?php echo get_template_directory_uri().'/img/intro-box/intro-box4.png' ?>">
+				<h4>Sin riesgo</h4>
+				<p>Estás con los expertos. Hemos inspirado a más de 230 organizaciones e impactado a más de 97,000 personas.</p>
+			</div>
+		</section>
 	</div>
-</section>
-
-<section class="boxes">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-4">
-				<?php if( is_active_sidebar( 'box1') ) : ?>
-					<?php dynamic_sidebar( 'box1' ); ?>
-				<?php endif; ?>
-			</div>
-			<div class="col-md-4">
-				<?php if( is_active_sidebar( 'box2') ) : ?>
-					<?php dynamic_sidebar( 'box2' ); ?>
-				<?php endif; ?>
-			</div>
-			<div class="col-md-4">
-				<?php if( is_active_sidebar( 'box3') ) : ?>
-					<?php dynamic_sidebar( 'box3' ); ?>
-				<?php endif; ?>
-			</div>
+	
+	<section class="destacado">
+		<div class="container destacado-content">	
+			<p><b>¿Te imaginas un evento en el que se mejoren las relaciones entre tus asistentes y se maximice la comunicación entre ellos?</b></p>
+			<p>Nosotros te ayudamos a logralo. Convertimos tu próximo evento en uno que inspire e impacte basándonos en estrategias de Ciencias de la Felicidad.</p>
 		</div>
+	</section>
+	
+	<div class="container front-page">		
+		<section class="reviews">
+			<div class="review">
+				<div class="review-comment">
+					<img class="quote-open" src="<?php echo get_template_directory_uri().'/img/quotes/quote-open.png' ?>">
+					<div class="review-text">
+						<span>Comprendieron perfectamente cuál era mi necesidad y cómo quería impactar en cada uno de mi eventos.</span>
+						<p>Gracias a Call2action actualmente tenemos el doble de asistentes en comparación con años anteriores debido a los buenos resultados.</p>
+					</div>
+					<img class="quote-close" src="<?php echo get_template_directory_uri().'/img/quotes/quote-close.png' ?>">
+				</div>
+				<div class="review-author">
+					<p>
+						<span>Samantha García Bernal</span></br>
+						Hospitals Marketing Coordinator</br>
+						<span>PEH BIOH Pﬁzer México</span>
+					</p>
+				</div>
+			</div>
+			<a href="contacto" class="btn-custom-green">Contáctanos</a>
+		</section>
 	</div>
-</section>
+</div>
+
 <?php get_footer(); ?>
